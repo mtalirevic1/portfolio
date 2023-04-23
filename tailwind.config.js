5d91e96a633c8fedb4381773084e2ab06a98ca2d
Matej Talirevic <matej@bloola.com>
2023-04-23 12:02:49 +0200

    added google fonts, removed default frontpage code

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        main: ['var(--font-roboto)'],
+        accent: ['var(--font-lato)']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
