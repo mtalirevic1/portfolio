ac8293179f0437c9718300517f4cd6c36ea05d3c
Matej Talirevic <talirevic.matej@gmail.com>
2024-07-20 18:01:37 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {Canvas} from "@react-three/fiber";
+import Experience from "./components/Experience.tsx";
+
+function App() {
+  return (
+      <Canvas
+          shadows
+          camera={{
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [2.5, 4, 6]
+          }}
+      >
+        <Experience/>
+      </Canvas>
+  )
+}
+
+export default App
