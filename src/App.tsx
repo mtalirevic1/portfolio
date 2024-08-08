daaacfdd0f43662dd1b2d5320b66c137b3b61031
Matej Talirevic <talirevic.matej@gmail.com>
2024-08-08 02:02:55 +0200

    char and map test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import {Canvas} from "@react-three/fiber";
 import Experience from "./components/Experience.tsx";
+import {KeyboardControls} from "@react-three/drei";
+
+const keyboardMap = [
+    { name: "forward", keys: ["ArrowUp", "KeyW"] },
+    { name: "backward", keys: ["ArrowDown", "KeyS"] },
+    { name: "left", keys: ["ArrowLeft", "KeyA"] },
+    { name: "right", keys: ["ArrowRight", "KeyD"] },
+    { name: "run", keys: ["Shift"] },
+];
 
 function App() {
     return (
-        <Canvas
-            shadows
-            camera={{
-                position: [0, 0, 10],
-                fov: 75
-            }}
-        >
-            <Experience/>
-        </Canvas>
+        <KeyboardControls map={keyboardMap}>
+            <Canvas
+                shadows
+                camera={{ position: [3, 3, 3], near: 0.1, fov: 40 }}
+                style={{
+                    touchAction: "none",
+                }}
+            >
+                <Experience/>
+            </Canvas>
+        </KeyboardControls>
     )
 }
 
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,13 +1,44 @@
-import {Character} from "./Character.tsx";
-import {OrbitControls} from "@react-three/drei";
+import {Environment, OrthographicCamera} from "@react-three/drei";
+import {useRef} from "react";
+import {Physics, RigidBody} from "@react-three/rapier";
+import {CharacterController} from "./CharacterController.tsx";
 
 const Experience = () => {
+    const shadowCameraRef = useRef(null);
     return (
         <>
             <color attach="background" args={['skyblue']}/>
-                <Character position={[0, 0, 0]} />
-                <ambientLight />
-                <OrbitControls />
+            <Environment preset="sunset"/>
+            <directionalLight
+                intensity={0.65}
+                castShadow
+                position={[-15, 10, 15]}
+                shadow-mapSize-width={2048}
+                shadow-mapSize-height={2048}
+                shadow-bias={-0.00005}
+            >
+                <OrthographicCamera
+                    left={-22}
+                    right={15}
+                    top={10}
+                    bottom={-20}
+                    ref={shadowCameraRef}
+                    attach={"shadow-camera"}
+                />
+            </directionalLight>
+            <Physics debug>
+                <RigidBody
+                    colliders='cuboid'
+                    position={[0, -3, 0]}
+                    rotation={[-Math.PI / 2, 0, 0]}
+                    type='fixed'>
+                    <mesh receiveShadow>
+                        <planeGeometry args={[1000, 1000]}/>
+                        <meshStandardMaterial color="#f0f0f0"/>
+                    </mesh>
+                </RigidBody>
+                <CharacterController/>
+            </Physics>
         </>
     )
 }
